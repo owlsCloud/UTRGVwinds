3913dc42d96da2772e2bc22e6e17cd919e54031e
Gerardo Garza <owlscloud@gmail.com>
2022-11-01 00:11:07 -0500

    banner decent

--- utrgvwinds/src/Components/Banner.jsx
+++ utrgvwinds/src/Components/Banner.jsx
@@ -3,7 +3,7 @@ import banner from "../assets/logo.png";
 function Banner() {
   return (
     <div className="flex justify-center ">
-      <img src={banner} className="h-44 w-1/3" alt="UTRGVwinds" />
+      <img src={banner} className="h-72 w-1/3" alt="UTRGVwinds" />
     </div>
   );
 }
