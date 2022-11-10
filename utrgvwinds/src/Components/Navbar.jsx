32ac52ddd85f96deb07c944129018da857cf7a3a
Gerardo Garza <owlscloud@gmail.com>
2022-11-10 13:32:45 -0600

    nav styling

--- utrgvwinds/src/Components/Navbar.jsx
+++ utrgvwinds/src/Components/Navbar.jsx
@@ -3,26 +3,26 @@ import React from "react";
 function Navbar() {
   return (
     <div className="w-full">
-      <ul className="flex justify-between">
-        <li className="text-utrgvOrange text-sm ">
+      <ul className="flex justify-between ">
+        <li className="text-utrgvOrange text-xl m-auto ">
           <a href="#a">Home</a>
         </li>
-        <li className="text-utrgvOrange text-sm">
+        <li className="text-utrgvOrange text-xl m-auto">
           <a href="#a">About</a>
         </li>
-        <li className="text-utrgvOrange text-sm">
+        <li className="text-utrgvOrange text-xl m-auto">
           <a href="#a">Percussion</a>
         </li>
-        <li className="text-utrgvOrange text-sm">
-          <a href="#a">winds</a>
+        <li className="text-utrgvOrange text-xl m-auto">
+          <a href="#a">Winds</a>
         </li>
-        <li className="text-utrgvOrange text-sm">
+        <li className="text-utrgvOrange text-xl m-auto ">
           <a href="#a">Colorguard</a>
         </li>
-        <li className="text-utrgvOrange text-sm">
+        <li className="text-utrgvOrange text-xl m-auto">
           <a href="#a">Contact Us</a>
         </li>
-        <li className="text-utrgvOrange text-sm">
+        <li className="text-utrgvOrange text-xl m-auto">
           <a href="#a">Store</a>
         </li>
       </ul>
